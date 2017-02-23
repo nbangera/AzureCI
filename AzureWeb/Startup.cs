b1276ca6f2e487928d093a7c6631ef7da0e1609d
nishank.bangera <nishank.bangera@RAV-LAP-482>
2017-02-23 19:10:21 +0530

    Add project files.

---
+++ AzureWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWeb.Startup))]
+namespace AzureWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
